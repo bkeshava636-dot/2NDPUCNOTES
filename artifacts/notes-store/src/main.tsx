310c81ab906324145d059c3453375f7073b0727c
bkeshava636-dot <bkeshava636@gmail.com>
2026-07-18 12:56:47 +0000

    Add login

--- artifacts/notes-store/src/main.tsx
+++ artifacts/notes-store/src/main.tsx
@@ -1,8 +1,15 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import { setAdminTokenGetter } from "@workspace/api-client-react";
+
+import {
+  setAdminTokenGetter,
+  setBaseUrl,
+} from "@workspace/api-client-react";
+
 import { getAdminToken } from "./lib/utils";
+
+setBaseUrl(import.meta.env.VITE_API_URL);
 
 setAdminTokenGetter(() => getAdminToken());
 
