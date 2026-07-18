b878cc70851ee9da291bd98bcb3eb3c4e560bf9e
bkeshava636-dot <bkeshava636@gmail.com>
2026-07-18 16:28:17 +0000

    Fix admin upload API URL

--- artifacts/notes-store/src/main.tsx
+++ artifacts/notes-store/src/main.tsx
@@ -1,16 +1,3 @@
-import { createRoot } from "react-dom/client";
-import App from "./App";
-import "./index.css";
-
-import {
-  setAdminTokenGetter,
-  setBaseUrl,
-} from "@workspace/api-client-react";
-
-import { getAdminToken } from "./lib/utils";
+console.log("API URL =", import.meta.env.VITE_API_URL);
 
 setBaseUrl(import.meta.env.VITE_API_URL);
-
-setAdminTokenGetter(() => getAdminToken());
-
-createRoot(document.getElementById("root")!).render(<App />);
